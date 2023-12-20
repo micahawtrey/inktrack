d5463a37ed428301d34c7377bcf82a31a2f6238b
Micah <micah.a.awtrey@gmail.com>
2023-12-20 14:19:40 -0700

    Video to Image Integration Complete

--- documents/static/js/documents.js
+++ documents/static/js/documents.js
@@ -14,31 +14,45 @@
     // The various HTML elements we need to configure or control. These
     // will be set by the startup() function.
 
-    var video = null;
     var canvas = null;
-    var photo = null;
+    var frontphoto = null;
     var formsrc = null;
-    var startbutton = null;
+    var frontbutton = null;
+    var frontcamera = null;
+    var frontIdbutton = null;
+    var backcamera = null;
+    var backIdbutton = null;
+    var backphoto = null;
+    var backbutton = null;
 
     function startup() {
-      video = document.getElementById('video');
+      frontvideo = document.getElementById('frontvideo');
+      backvideo = document.getElementById('backvideo');
       canvas = document.getElementById('canvas');
-      photo = document.getElementById('photo');
+      frontphoto = document.getElementById('frontphoto');
       formsrc = document.getElementById('webimg');
-      startbutton = document.getElementById('startbutton');
+      frontbutton = document.getElementById('frontbutton');
+      frontcamera = document.getElementById('frontcamera');
+      frontIdbutton = document.getElementById('frontId');
+      backcamera = document.getElementById('backcamera');
+      backIdbutton = document.getElementById('backId');
+      backIdretake = document.getElementById('backIdretake');
+      backphoto = document.getElementById('backphoto');
+      backbutton = document.getElementById('backbutton');
 
-      navigator.mediaDevices.getUserMedia({video: true, audio: false})
-      .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function(err) {
-        console.log("An error occurred: " + err);
-      });
+      frontIdbutton.addEventListener('click', function(ev){
+        displaycamera(frontcamera, frontphoto);
+        idpicture(frontvideo);
+        }, false);
 
-      video.addEventListener('canplay', function(ev){
+      backIdbutton.addEventListener('click', function(ev){
+        displaycamera(backcamera, backphoto);
+        idpicture(backvideo);
+        }, false);
+
+      frontvideo.addEventListener('canplay', function(ev){
         if (!streaming) {
-          height = video.videoHeight / (video.videoWidth/width);
+          height = frontvideo.videoHeight / (frontvideo.videoWidth/width);
 
           // Firefox currently has a bug where the height can't be read from
           // the video, so we will make assumptions if this happens.
@@ -47,26 +61,66 @@
             height = width / (4/3);
           }
 
-          video.setAttribute('width', width);
-          video.setAttribute('height', height);
+          frontvideo.setAttribute('width', width);
+          frontvideo.setAttribute('height', height);
           canvas.setAttribute('width', width);
           canvas.setAttribute('height', height);
           streaming = true;
         }
       }, false);
 
-      startbutton.addEventListener('click', function(ev){
-        takepicture();
+      backvideo.addEventListener('canplay', function(ev){
+        if (!streaming) {
+          height = backvideo.videoHeight / (backvideo.videoWidth/width);
+
+          // Firefox currently has a bug where the height can't be read from
+          // the video, so we will make assumptions if this happens.
+
+          if (isNaN(height)) {
+            height = width / (4/3);
+          }
+
+          backvideo.setAttribute('width', width);
+          backvideo.setAttribute('height', height);
+          canvas.setAttribute('width', width);
+          canvas.setAttribute('height', height);
+          streaming = true;
+        }
+      }, false);
+
+      frontbutton.addEventListener('click', function(ev){
+        takepicture(frontvideo, frontphoto, frontIdbutton, frontcamera);
         ev.preventDefault();
       }, false);
 
-      clearphoto();
+      backbutton.addEventListener('click', function(ev){
+        takepicture(frontvideo, backphoto, backIdbutton, backcamera);
+        ev.preventDefault();
+      }, false);
+
+      // clearphoto();
+    }
+
+    function displaycamera(camera, photo) {
+      camera.style.display = 'block';
+      photo.style.display = 'block';
+    }
+
+    function idpicture(idvideo) {
+      navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      .then(function(stream) {
+        idvideo.srcObject = stream;
+        idvideo.play();
+      })
+      .catch(function(err) {
+        console.log("An error occurred: " + err);
+      });
     }
 
     // Fill the photo with an indication that none has been
     // captured.
 
-    function clearphoto() {
+    function clearphoto(photo) {
       var context = canvas.getContext('2d');
       context.fillStyle = "#AAA";
       context.fillRect(0, 0, canvas.width, canvas.height);
@@ -81,7 +135,7 @@
     // drawing that to the screen, we can change its size and/or apply
     // other changes before drawing it.
 
-    function takepicture() {
+    function takepicture(video, photo, idbutton, camera) {
       var context = canvas.getContext('2d');
       if (width && height) {
         canvas.width = width;
@@ -91,8 +145,13 @@
         var data = canvas.toDataURL('image/png');
         photo.setAttribute('src', data);
         formsrc.setAttribute('value', data);
-      } else {
-        clearphoto();
+        idbutton.innerHTML = "Retake ID photo";
+        camera.style.display = "none";
+        streaming = false;
+
+      }
+      else {
+        clearphoto(photo);
       }
     }
 
