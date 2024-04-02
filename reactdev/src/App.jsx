09112420f013b2237497f079e8bfbcdcdd351b40
Micah <micah.a.awtrey@gmail.com>
2024-04-02 08:34:46 -0600

    Footer created

--- reactdev/src/App.jsx
+++ reactdev/src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Footer from "./components/footer"
 
 export default function App() {
   return (
     <div>
       <Outlet />
+      <Footer />
     </div>
   )
 }
